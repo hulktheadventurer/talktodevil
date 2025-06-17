273325cb299d5f8caccbaeb27b5deed06228c91b
hulktheadventurer <hulktheadventurer@gmail.com>
2025-06-18 00:26:43 +0100

    Fix og:image URL to og-image.png

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Confessly</title>
       <meta property="og:title" content="Confessly" />
       <meta property="og:description" content="A sacred space for your digital confessions." />
-      <meta property="og:image" content="/og-image.png" />
+      <meta property="og:image" content="https://www.confessly.life/og-image.png" />
       <meta property="og:url" content="https://www.confessly.life/" />
       <meta property="og:type" content="website" />
     </>
